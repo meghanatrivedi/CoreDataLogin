b92f2ba740e310c9cadfc985e2d1130f99b489ff
Meghana Trivedi <meghanatrivedi92@gmail.com>
2022-05-17 17:43:14 +0530

    Initial Commit

---
+++ CoreDataLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataLogin
+//
+//  Created by Meghna on 17/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
